93a3f31e541b9490446b14dbdfd3c12b50c0ce21
Thomas Arni <thomas.arni@gmail.com>
2012-08-19 09:44:35 +0200

    Prepare the Task model: add case class Task / add view template / extend Application controller with taskForm.

--- todolist/app/controllers/Application.scala
+++ todolist/app/controllers/Application.scala
@@ -2,17 +2,26 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
 
 object Application extends Controller {
-  
+
+  import play.api.data._
+  import play.api.data.Forms._
+
+  val taskForm = Form(
+    "label" -> nonEmptyText)
+
   def index = Action {
     Redirect(routes.Application.tasks)
   }
-  
-   def tasks = TODO
-  
+
+  def tasks = Action {
+	  Ok(views.html.index(Task.all(), taskForm))
+  }
+
   def newTask = TODO
-  
+
   def deleteTask(id: Long) = TODO
-  
+
 }
